7ff4058e65b40eba86ad0a1377f5d8395d76c7b6
Sergio Antonio <111243440+sergioantonioar@users.noreply.github.com>
2026-09-13 19:52:58 -0500

    add shootfire, gun & other animation

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,14 +2,54 @@ extends CharacterBody2D
 
 var speed=125
 
+var health = 100
+
 @onready var anim= $AnimationPlayer
+
+var is_hurt = false
+
+func _ready():
+	$AnimationPlayer.animation_finished.connect(_on_animation_finished)
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
 	velocity=direction*speed
 	move_and_slide()
 	
-	if direction.length() > 0:
-		anim.play("Walk")
+	if(is_hurt==false):
+		if direction.length() > 0:
+			anim.play("Walk")
+		else:
+			anim.play("Idle")
+		
+	var mouse_pos = get_global_mouse_position()
+	
+	if mouse_pos.x < global_position.x:
+		$Sprite2D.flip_h = true
 	else:
-		anim.play("Idle")
+		$Sprite2D.flip_h = false
+		
+	var overlapping_enemies = $HurtZone.get_overlapping_bodies()
+	for enemy in overlapping_enemies:
+		if enemy.is_in_group("Enemies"):
+			take_damage(1)
+			break
+
+func take_damage(amount):
+	if is_hurt:
+		return
+	
+	health-=amount
+	print("Player recibe daño, salud ahora: ", health)
+	is_hurt=true
+	anim.play("Hurt")
+	if health<=0:
+		die()
+
+func _on_animation_finished(anim_name):
+	if(anim_name=="Hurt"):
+		is_hurt=false
+
+func die():
+	print("Player muerto, reiniciamos nivel")
+	get_tree().reload_current_scene()
